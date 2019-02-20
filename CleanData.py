84c053308048cd9474d45949e508c4e21ac368de
Andrew Smith <andrewts129@gmail.com>
2019-02-20 16:32:31 -0500

    added incomplete method for matching building shapes

--- CleanData.py
+++ CleanData.py
@@ -52,21 +52,17 @@ def load_parcels():
                 parcel_address = "TODO"
                 parcel_shape = geometry.shape(feature["geometry"])
 
-                new_feature = geojson.Feature(
-                    geometry=parcel_shape,
-                    properties={
-                        "id": parcel_id,
-                        "address": parcel_address,
-                        "year_built": parcel_year_built
-                    }
-                )
+                new_feature = {"geometry": parcel_shape,
+                               "id": parcel_id,
+                               "address": parcel_address,
+                               "year_built": parcel_year_built}
 
                 parcel_features.append(new_feature)
 
                 if len(parcel_features) % 10000 == 0:
                     print("Loaded " + str(len(parcel_features)) + " parcels...")
 
-    return geojson.FeatureCollection(parcel_features)
+    return parcel_features
 
 
 def load_buildings():
@@ -83,15 +79,63 @@ def load_buildings():
     return building_shapes
 
 
+def match_buildings_to_parcels(parcel_features, building_shapes):
+    # TODO remove this once I can actually get the code below to terminate before the heat death of the universe
+    return parcel_features
+
+    good_matches = 0
+    partial_matches = 0
+    non_matches = 0
+
+    building_features = []
+
+    for building_shape in building_shapes:
+        full_match = False
+        matching_parcel = None
+
+        for parcel_feature in parcel_features:
+            if parcel_feature["geometry"].contains(building_shape):
+                matching_parcel = parcel_feature
+                full_match = True
+                break
+            elif parcel_feature["geometry"].intersects(building_shape):
+                matching_parcel = parcel_feature
+
+        if matching_parcel is not None:
+            new_feature = geojson.Feature(geometry=building_shape,
+                                          properties={"id": matching_parcel["id"],
+                                                      "address": matching_parcel["address"],
+                                                      "year_built": matching_parcel["year_built"]})
+            building_features.append(new_feature)
+
+            if full_match:
+                good_matches += 1
+            else:
+                partial_matches += 1
+
+            if len(building_features) % 100 == 0:
+                print("Matched " + str(len(building_features)) + " buildings to features. (" + str(good_matches) + " full, " + str(partial_matches) + " partial, " + str(non_matches) + " failures)")
+        else:
+            non_matches += 1
+
+            if non_matches % 10 == 0:
+                print("(" + str(non_matches) + " failures in matching so far)")
+
+    return geojson.FeatureCollection(building_features)
+
+
 def main():
-    parcels = load_parcels()
+    parcel_features = load_parcels()
     print("Finished loading parcels!")
 
-    buildings = load_buildings()
+    building_shapes = load_buildings()
     print("Finished loading buildings!")
 
+    building_features = match_buildings_to_parcels(parcel_features, building_shapes)
+    print("Finished matching buildings to parcels!")
+
     with open(GEOJSON_OUT, "w") as file:
-        geojson.dump(parcels, file)
+        geojson.dump(building_features, file)
 
     tippecanoe_command = "tippecanoe -o " + MBTILES_OUT + " -zg --drop-densest-as-needed --force " + GEOJSON_OUT
     subprocess.call(tippecanoe_command.split(" "), stderr=sys.stderr, stdout=sys.stdout)
