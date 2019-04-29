7a75ecf4b6a25c81a406d9a9fcb343a2f673e115
Andrew Smith <andrewts129@gmail.com>
2019-04-28 21:55:48 -0400

    legend

--- viz/viz.js
+++ viz/viz.js
@@ -9,12 +9,6 @@ window.onload = function() {
     });
 
     map.on("load", function () {
-        map.addSource("columbus", {
-            id: "columbus",
-            type: "vector",
-            tiles:["http://localhost:8080/data/columbus/{z}/{x}/{y}.pbf"]
-        });
-
         map.addSource("buildings", {
             id: "buildings",
             type: "vector",
@@ -23,9 +17,18 @@ window.onload = function() {
             maxzoom: 15,
         });
 
+        map.addSource("columbus", {
+            id: "columbus",
+            type: "vector",
+            tiles:["http://localhost:8080/data/columbus/{z}/{x}/{y}.pbf"]
+        });
+
         const min_year = 1800;
         const max_year = 2019;
         const colors = ["#e41a1c", "#ff7f00", "#ffff33", "#4daf4a", "#377eb8"];
+
+        const unavailable_year = 0;
+        const unavailable_color = "#eaeae5";
 
         map.addLayer({
             id: "buildings",
@@ -35,11 +38,11 @@ window.onload = function() {
             paint: {
                 "fill-color": {
                     "property": "year_built",
-                    "stops": [[0, "#eaeae5"]].concat(colors.map(function (color, index) {
+                    "stops": [[unavailable_year, unavailable_color]].concat(colors.map(function (color, index) {
                         return [min_year + (index * (max_year - min_year) / (colors.length - 1)), color]
                     }))
                 }
             }
-        })
+        });
     })
 };
