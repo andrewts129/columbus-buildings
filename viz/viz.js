bcf038a78c1b9a7482b0f971d3e8f2ce1c476108
Andrew Smith <andrewts129@gmail.com>
2019-06-24 21:55:13 -0400

    updated viz

--- viz/viz.js
+++ viz/viz.js
@@ -1,63 +1,72 @@
 window.onload = function() {
-    let map = new mapboxgl.Map({
-        container: 'map',
-        center: [-82.9988, 39.9612],
-        zoom: 12,
-        minZoom: 12,
-        maxZoom: 18,
-        style: "http://localhost:8080/styles/base/style.json"
-    });
+    if (!mapboxgl.supported()) {
+        window.alert("Sorry, your browser doesn't support the map on this page")
+    }
+    else {
+        const tileServerURL = "http://localhost:8080";
 
-    map.on("load", function () {
-        map.addSource("buildings", {
-            id: "buildings",
-            type: "vector",
-            tiles:["http://localhost:8080/data/data/{z}/{x}/{y}.pbf"],
-            minzoom: 12,
-            maxzoom: 15,
+        let map = new mapboxgl.Map({
+            container: 'map',
+            center: [-82.9988, 39.9612],
+            zoom: 12,
+            minZoom: 12,
+            maxZoom: 18,
+            style: tileServerURL + "/styles/base/style.json",
+            antialias: true,
         });
 
-        map.addSource("columbus", {
-            id: "columbus",
-            type: "vector",
-            tiles:["http://localhost:8080/data/columbus/{z}/{x}/{y}.pbf"]
-        });
+        map.on("load", function () {
+            map.addSource("buildings", {
+                id: "buildings",
+                type: "vector",
+                tiles:[tileServerURL + "/data/data/{z}/{x}/{y}.pbf"],
+                minzoom: 12,
+                maxzoom: 15,
+            });
 
-        const min_year = 1800;
-        const max_year = 2019;
-        const colors = ["#e41a1c", "#ff7f00", "#ffff33", "#4daf4a", "#377eb8"];
+            map.addSource("columbus", {
+                id: "columbus",
+                type: "vector",
+                tiles:[tileServerURL + "/data/columbus/{z}/{x}/{y}.pbf"]
+            });
 
-        const unavailable_year = 0;
-        const unavailable_color = "#eaeae5";
+            map.addControl(new mapboxgl.FullscreenControl());
 
-        const dated_building_layer_id = "dated_buildings";
-        const undated_building_layer_id = "undated_buildings";
+            const colors = ["#e41a1c", "#f24d0e", "#ff7f00", "#FFBF1A", "#ffff33", "#A6D73F", "#4daf4a", "#429781", "#377eb8", "#6866AE", "#984ea3"];
+            const stopYears = [1800, 1825, 1850, 1875, 1900, 1925, 1950, 1975, 2000, 2025, 2050];
 
-        map.addLayer({
-            id: dated_building_layer_id,
-            type: "fill",
-            source: "buildings",
-            "source-layer": "data",
-            filter: ["!=", "year_built", unavailable_year],
-            paint: {
-                "fill-color": {
-                    "property": "year_built",
-                    "stops": colors.map(function (color, index) {
-                        return [min_year + (index * (max_year - min_year) / (colors.length - 1)), color]
-                    })
+            const unavailable_year = 0;
+            const unavailable_color = "#eaeae5";
+
+            const dated_building_layer_id = "dated_buildings";
+            const undated_building_layer_id = "undated_buildings";
+
+            map.addLayer({
+                id: dated_building_layer_id,
+                type: "fill",
+                source: "buildings",
+                "source-layer": "data",
+                filter: ["!=", "year_built", unavailable_year],
+                paint: {
+                    "fill-color": {
+                        "property": "year_built",
+                        "stops": colors.map(function (color, index) {
+                            return [stopYears[index], color]
+                        })
+                    }
                 }
-            }
-        });
+            });
 
-        map.addLayer({
-            id: undated_building_layer_id,
-            type: "fill",
-            source: "buildings",
-            "source-layer": "data",
-            filter: ["==", "year_built", 0],
-            paint: {
-                "fill-color": unavailable_color
-            }
-        }, "dated_buildings");
-    })
+            map.addLayer({
+                id: undated_building_layer_id,
+                type: "fill",
+                source: "buildings",
+                "source-layer": "data",
+                filter: ["==", "year_built", unavailable_year],
+                paint: {
+                    "fill-color": unavailable_color
+                }
+            }, "dated_buildings");
+        })
+    }
 };
