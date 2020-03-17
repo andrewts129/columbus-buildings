23f77893c3cad3d6091569166a27a7c69d152803
Andrew Smith <andrewts129@gmail.com>
2020-03-17 15:49:41 -0400

    removed intersection checks:

--- GenerateTiles.py
+++ GenerateTiles.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import itertools
 import json
 import logging
 import os
@@ -7,23 +6,17 @@ import re
 import subprocess
 import sys
 import tempfile
-import multiprocessing as mp
-import time
 from concurrent.futures.thread import ThreadPoolExecutor
 from ftplib import FTP
-from multiprocessing.managers import Namespace
 from pathlib import Path
-from typing import Iterable, Hashable, Dict, Tuple, Optional
+from typing import Iterable, Hashable, Dict
 from zipfile import ZipFile
 
 import fiona
 import geopandas as gpd
 import pandas as pd
 import requests
-from geopandas import GeoDataFrame, GeoSeries
-from shapely.geometry import Polygon
-from shapely.prepared import prep
-from shapely.strtree import STRtree
+from geopandas import GeoDataFrame
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -99,18 +92,19 @@ def load_parcels(parcel_file_name: str) -> GeoDataFrame:
     with fiona.open(parcel_file_name) as features:
         properties_to_keep = ['PARCELID', 'RESYRBLT']
         slim_features = features_slimmed(features, properties_to_keep)  # Reduces memory usage
-        slim_features = (f for f in slim_features if
-                         f['geometry'] is not None)  # Apparently there are things in here with no shape?
-        df = GeoDataFrame.from_features(slim_features)
-        df.crs = features.crs
+        slim_features = (f for f in slim_features if f['geometry'] is not None)  # Apparently there are things in here with no shape?
+        gdf = GeoDataFrame.from_features(slim_features)
+        gdf.crs = features.crs
 
-    return df.to_crs(epsg=4326)
+    return gdf.to_crs(epsg=4326)
 
 
 def download_osu_buildings_ages(building_file_name: str, data_dir: str) -> GeoDataFrame:
     def fetch_building_age(building_number: str) -> int:
         response = requests.get(
-            f"https://gismaps.osu.edu/OSUDataService/OSUService.svc/BuildingDetailsExtended/{building_number}")
+            f"https://gismaps.osu.edu/OSUDataService/OSUService.svc/BuildingDetailsExtended/{building_number}"
+        )
+
         build_year_group = re.search('"Date Constructed":"(\\d*)/', response.text)
         if build_year_group is None:
             return 0
@@ -143,14 +137,14 @@ def download_osu_buildings_ages(building_file_name: str, data_dir: str) -> GeoDa
 
 def load_osu_buildings(building_file_name: str, data_dir: str) -> GeoDataFrame:
     with fiona.open(building_file_name) as features:
-        df = GeoDataFrame.from_features(features)
-        df.crs = features.crs
+        gdf = GeoDataFrame.from_features(features)
+        gdf.crs = features.crs
 
     building_ages = download_osu_buildings_ages(building_file_name, data_dir)
-    df = df.merge(building_ages, on='BLDG_NUM')
-    df = df[['geometry', 'year_built']]  # Remove all the columns we don't need
+    gdf = gdf.merge(building_ages, on='BLDG_NUM')
+    gdf = gdf[['geometry', 'year_built']]  # Remove all the columns we don't need
 
-    return df.to_crs(epsg=4326)
+    return gdf.to_crs(epsg=4326)
 
 
 def contains_letters(s: str) -> bool:
@@ -168,68 +162,18 @@ def clean_parcel_id(s: str) -> str:
         return s.strip().replace('-', '')
 
 
-def clean_parcel_data_frame(df: GeoDataFrame) -> GeoDataFrame:
-    new_df = df.rename(columns={'PARCELID': 'parcel_id', 'RESYRBLT': 'year_built'})
+def clean_parcel_data_frame(gdf: GeoDataFrame) -> GeoDataFrame:
+    new_gdf = gdf.rename(columns={'PARCELID': 'parcel_id', 'RESYRBLT': 'year_built'})
 
-    new_df = new_df[~new_df.parcel_id.apply(contains_letters)]
-    new_df.parcel_id = new_df.parcel_id.apply(clean_parcel_id)
-    new_df = new_df[new_df.year_built.apply(sane_year_built)]
+    new_gdf = new_gdf[~new_gdf.parcel_id.apply(contains_letters)]
+    new_gdf.parcel_id = new_gdf.parcel_id.apply(clean_parcel_id)
+    new_gdf = new_gdf[new_gdf.year_built.apply(sane_year_built)]
 
-    return new_df
-
-
-def concat(gdf1: GeoDataFrame, gdf2: GeoDataFrame) -> GeoDataFrame:
-    return GeoDataFrame(pd.concat([gdf1, gdf2], ignore_index=True), crs=gdf1.crs)
+    return new_gdf
 
 
-# Returns None if the shape intersects with a dated building, the shape otherwise
-def remove_if_intersects_with_dated(args: Tuple[Polygon, Namespace]) -> Optional[Polygon]:
-    undated_building_shape, shared_memory = args
-    close_dated_shapes = shared_memory.tree.query(undated_building_shape.buffer(0.001))
-
-    undated_building_shape_prep = prep(undated_building_shape)
-
-    for close_dated_shape in close_dated_shapes:
-        if undated_building_shape_prep.intersects(close_dated_shape):
-            return None
-
-    return undated_building_shape  # Doesn't intersect with anything
-
-
-# TODO this approach sucks
-def filter_intersecting_undated_buildings(df: GeoDataFrame) -> GeoDataFrame:
-    dated_mask = df['year_built'] > 0
-    dated = df[dated_mask]
-    undated = df[~dated_mask]
-
-    dated_shape_tree = STRtree(dated.geometry)
-    logger.debug('made tree')
-
-    start = time.time()
-
-    filtered_undated_shapes = []
-    for i, undated_building_shape in enumerate(undated.geometry):
-        close_dated_shapes = dated_shape_tree.query(undated_building_shape.buffer(0.0001))
-
-        # Only prep if there are multiple nearby shapes to check
-        undated_building_shape_prep = prep(undated_building_shape) if len(close_dated_shapes) > 1 else undated_building_shape
-
-        keep = True
-        for close_dated_shape in close_dated_shapes:
-            if undated_building_shape_prep.intersects(close_dated_shape):
-                keep = False
-                break
-
-        if keep:
-            filtered_undated_shapes.append(undated_building_shape)
-        if i % 1000 == 0:
-            logger.debug(f'Discarded {i - len(filtered_undated_shapes) + 1} undated buildings, kept {len(filtered_undated_shapes)}')
-        if i == 5000:
-            logger.info(f'That took {time.time() - start}')
-
-    # Rebuilds the dataframe
-    filtered_undated = GeoDataFrame(zip(filtered_undated_shapes, itertools.repeat(0)), columns=['geometry', 'year_built'])
-    return concat(dated, filtered_undated)
+def concat(*dataframes: GeoDataFrame) -> GeoDataFrame:
+    return GeoDataFrame(pd.concat([*dataframes], ignore_index=True), crs=dataframes[0].crs)
 
 
 def main():
@@ -281,17 +225,10 @@ def main():
 
     logger.info('Joined data...')
 
-    final_df = filter_intersecting_undated_buildings(combined_df)
-    logger.debug(final_df.head())
-    logger.debug(final_df.info())
-    logger.debug(final_df.describe())
-
-    logger.info('Removed duplicates...')
-
     # TODO do something about duplicate buildings
     output_geojson_file_name = f'{data_dir}/buildings.geojson'
     output_mbtiles_file_name = f'{data_dir}/buildings.mbtiles'
-    final_df.to_file(output_geojson_file_name, driver='GeoJSON')
+    combined_df.to_file(output_geojson_file_name, driver='GeoJSON')
     subprocess.call(['bash', 'tippecanoe_cmd.sh', output_mbtiles_file_name, output_geojson_file_name],
                     stderr=sys.stderr, stdout=sys.stdout)
 
